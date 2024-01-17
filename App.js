81960fc1589b48d55c9f49891ffec45d4bce01fa
Vishal Kumar <vishal.kumar65@ad.infosys.com>
2024-01-17 22:53:30 +0530

    episode 2 updated bundler for react

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")]),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")])
-]
-);
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
 
-
-
-const heading = React.createElement("h1", {}, "Hi World from react! ")
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = React.createElement("h1", {}, "Hi World from react! ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+# Parcel
+- Dev Build
+- Local server
+- HMR = hot module replacement
+- file watching algorithm -> written in C++
+- caching - faster builds
+- image optimastion
+- minification
+- building
+- compressing
