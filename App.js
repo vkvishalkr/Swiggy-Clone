8feb9f9aae65c87ad17de9dcd4dc6eaa1e74a375
Vishal Kumar <vishal.kumar65@ad.infosys.com>
2024-01-17 15:11:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")]),
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")])
+]
+);
+
+
+
+const heading = React.createElement("h1", {}, "Hi World from react! ")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+
+root.render(parent);
