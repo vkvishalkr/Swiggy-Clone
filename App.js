ed2b443f34e5177a2f106399ea74c22775b71276
Vishal Kumar <vishal.kumar65@ad.infosys.com>
2024-01-19 18:17:04 +0530

    component foundation covered

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+// const heading = React.createElement("h1", { id: "heading" }, "You are in🚀");
 
-const heading = React.createElement("h1", {}, "Hi World from react! ");
+const Heading = () => (
+  <h1 className="head" tabIndex="5">
+    You are in with JSX🚀
+  </h1>
+);
+
+const title = (
+  <h1>This is React Element.</h1>
+);
+
+// React functional component
+
+const HeadingComponent = () => {
+  return <h1 className="heading">This is react functional component</h1>;
+};
+
+// This is also react functional component
+const HeadingComponent2 = () => (
+  <div id="container">
+    <Heading />
+    {title}
+    <h1 className="heading">This is react functional component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-root.render(parent);
+root.render(<HeadingComponent2 />);
--- README.md
+++ README.md
@@ -4,9 +4,17 @@
 - Dev Build
 - Local server
 - HMR = hot module replacement
-- file watching algorithm -> written in C++
+- file watching algorithm - written in C++
 - caching - faster builds
 - image optimastion
 - minification
 - building
 - compressing
+- consistent hashing
+- code splitting
+- Differntial bundling - supports older version
+- Diagnostics
+- Error handling
+- HTTPs
+- Tree shaking - removes unused code
+- different dev and prod bundles
