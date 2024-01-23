3e27b1e36028aaeba8ed9b4956c26e6d6139f6ef
Vishal Kumar <vishal.kumar65@ad.infosys.com>
2024-01-23 19:59:15 +0530

    started food app project

--- App.js
+++ App.js
@@ -9,9 +9,7 @@ const Heading = () => (
   </h1>
 );
 
-const title = (
-  <h1>This is React Element.</h1>
-);
+const title = <h1>This is React Element.</h1>;
 
 // React functional component
 
@@ -28,6 +26,65 @@ const HeadingComponent2 = () => (
   </div>
 );
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const {resName, cuisine} = props;
+  return (
+    <div className="res-card">
+      <img className="res-logo" alt="res-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn5iC3e7B5FZ0c2YdjqmWyJU0E8sAD2Ry0Lw&usqp=CAU" />
+      <h3>{props.resName}</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4⭐</h4>
+      <h4>25 Minutes</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return(
+    <div className="body">
+      <div className="search">
+        search
+      </div>
+      <div className="res-container">
+        <RestaurantCard resName = "KFC"/>
+        <RestaurantCard resName="Meghana Foods"/>
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+}
+
+const AppLayout = () => {
+  return <div className="app">
+    <Header />
+    <Body />
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent2 />);
+root.render(<AppLayout />);
