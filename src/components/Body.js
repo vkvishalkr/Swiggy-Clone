d59a1a0d16479cfacdbb6415e72d65e7b363c427
Vishal <vkvishalkumar330@gmail.com>
2024-02-01 18:25:08 +0530

    swiggy api CORS Error Resolved

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,23 @@
 import RestaurantCard from "./Restaurantcard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    // setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  };
 
   return (
     <div className="body">
